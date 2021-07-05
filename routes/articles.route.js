14842a849bc767c36bb295ddd9ea8b1b412b6e29
kojusola <adeola5678@gmail.com>
2021-07-05 09:40:01 +0100

    Update articles.route.js

--- routes/articles.route.js
+++ routes/articles.route.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const { verifyToken } = require('../middleware/admin.authorization');
 const articleController = require('../controllers/articles.controller')
 
-router.post('/create',verifyToken, articleController.createArticle);
-router.post('/update',verifyToken, articleController.updateArticle);
-router.post('/updateImage',verifyToken, articleController.updateArticleImage);
-router.delete('/delete',verifyToken, articleController.deleteArticle);
+router.post('/create', articleController.createArticle);
+router.post('/update', articleController.updateArticle);
+router.post('/updateImage', articleController.updateArticleImage);
+router.delete('/delete', articleController.deleteArticle);
 router.get('/', articleController.getAllArticle);
 router.get('/single', articleController.getOneArticle);
-router.get('/publish',verifyToken, articleController.publishArticle);
+router.get('/publish', articleController.publishArticle);
 
 module.exports = router;
